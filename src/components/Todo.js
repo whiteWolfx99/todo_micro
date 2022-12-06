001acf8011e2a1468e685269ccac066c58c35b63
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-12-06 12:03:54 +0300

    todo list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function todo() {
+function Todo() {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState('');
+  const [isEdit, setIsedit] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todo !== '') {
+      const id = Math.floor(Math.random() * 1000);
+      const newTodo = { id, todo };
+      setTodos([...todos, newTodo]);
+      setTodo('');
+    } else {
+      alert('Please enter a todo');
+    }
+  };
+
+  const EditTask = () => {
+    if (editTodo !== '') {
+      const filterRecord = [...todos].filter(
+        (todo) => todo.id !== editTodo.id,
+      );
+      const editedobject = [...filterRecord, editTodo];
+      setTodos(editedobject);
+      setEditTodo('');
+    }
+    setIsedit(false);
+  };
+
+  const changeTask = (e) => {
+    const newEntry = {
+      id: editTodo.id,
+      todo: e.target.value,
+    };
+    setEditTodo(newEntry);
+  };
+
+  const handleDelete = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
   return (
-    <div>todo</div>
+    <div className="todo">
+
+      <div className="input-todo">
+        <input
+          placeholder="todo..."
+          type="text"
+          maxLength="10"
+          value={isEdit ? editTodo.todo : todo}
+          onChange={isEdit ? (e) => changeTask(e) : (e) => setTodo(e.target.value)}
+        />
+        <button type="button" onClick={isEdit ? EditTask : handleSubmit}>{isEdit ? 'save' : 'add'}</button>
+      </div>
+
+      <div className="content">
+        {todos.sort((a, b) => a.id - b.id).map((item) => (
+          <div key={item.id} className="todo-item">
+            <input type="checkbox" />
+            <p className="todo-paragraph">{item.todo}</p>
+            <div className="edit-delete">
+              <button
+                type="button"
+                onClick={() => {
+                  setEditTodo(item);
+                  setIsedit(true);
+                }}
+              >
+                Edit
+              </button>
+              <button onClick={() => handleDelete(item.id)} type="button">Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
-export default todo;
+export default Todo;
---
+++ src/components/pages/Firstabout.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function FirstAbout() {
+  return (
+    <div>
+      <div>
+        <h1>Welcome Developer</h1>
+        <p>Here you can find all the information about me</p>
+      </div>
+      <div>
+        <h2>name:</h2>
+        <p>Hevar Hoshang Ahmed</p>
+        <h2>age:</h2>
+        <p>22</p>
+        <h2>country:</h2>
+        <p>Iraq</p>
+        <h2>city:</h2>
+        <p>Hawler</p>
+        <h2>education:</h2>
+        <p>Software engineering</p>
+        <h2>skills:</h2>
+        <p>React, javascript, css, html, typscript, .netcore, vue.js</p>
+      </div>
+    </div>
+  );
+}
+
+export default FirstAbout;
